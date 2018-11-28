90998af9172b1464f12337c4c105623a6be97175
somoregie <somoregie@novomatic.com>
2018-11-28 16:07:36 +0100

    - moved files from partials to components folder and deleted partials - added dropdowns

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service'
+import { AppInfo } from '../../app.settings';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+   //#region keep the view bindables here (on top) for easy readability
+   
+   appInfo: any;
+   company: string     = this.company      ? this.company    : AppInfo.Company;
+   author: string      = this.author       ? this.author     : AppInfo.Author;
+   department: string  = this.department   ? this.department : AppInfo.Department;
+   version: string     = this.version      ? this.version    : AppInfo.Version;
+   copyright: string   = this.copyright    ? this.copyright  : AppInfo.Copyright;
+   environment: string = environment.isDev ? 'Dev' : '';
+  //  title: string       = AppInfo.Title;
+ 
+   //#endregion keep the view bindables here (on top) for easy readability
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.getAppInfo();
+  }
+
+  getAppInfo() {
+    this.dataService.getAppInfo().subscribe(
+      data => {
+        console.log('AppInfo from data service: ', data.GetAppInfoResult)
+        this.appInfo = data.GetAppInfoResult;
+
+        //#region iterate over the appInfo array 
+        this.appInfo.forEach((info: any) => {
+          //console.log(info.Author);
+          this.author     = info.Author;
+          this.company    = info.Company;
+          this.department = info.Department;
+          this.version    = info.Version;
+          this.copyright  = info.Copyright;
+          // this.title      = info.ProductTitle;
+        })
+        //#endregion iterate over the appInfo array 
+      },
+      err => console.error(err),
+      () => console.log('done fetching the application information')
+    )
+  }
+
+}
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,158 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+
+//#region variables and constants 
+
+restangularUsers: any = [];
+httpUsers: any = [];
+currentPage = 1;
+itemsPerPage: number = 10;
+pageSize: number;
+ChangePageSize: boolean;
+
+dropdownList: any = [];
+selectedItems: any = [];
+dropdownSettings: any = {};
+
+userSettings: any = {};
+
+//#endregion variables and constants 
+
+constructor(private dataService: DataService) { }
+
+//#region ngOnInit 
+
+  ngOnInit() {
+    this.getUsers();
+    this.getHttpUsers();
+    //this.getAppInfo();
+
+    //#region scrollHandle 
+
+window.onload = function(){
+      var tableCont = document.querySelector('#mainBody')
+      /**
+       * scroll handle
+       * @param {event} e -- scroll event
+       */
+      function scrollHandle (e){
+        var scrollTop = this.scrollTop;
+        this.querySelector('thead').style.transform = 'translateY(' + scrollTop + 'px)';
+      }
+      
+      tableCont.addEventListener('scroll',scrollHandle)
+    }
+
+//#endregion
+    
+    this.dropdownList = [
+      { item_id: 1, item_text: 'Mumbai' },
+      { item_id: 2, item_text: 'Bangaluru' },
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Navsari' },
+      { item_id: 5, item_text: 'New Delhi' }
+    ];
+    this.selectedItems = [
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Navsari' }
+    ];
+
+    this.userSettings = {
+      idField: 'id',
+      textField: 'first_name',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 1,
+      allowSearchFilter: true,
+      searchPlaceholderText: 'Filter...',
+      closeDropDownOnSelection: true,
+      clearSearchFilter: true,
+      placeholder: 'Select'
+    }
+
+    this.dropdownSettings = {
+      singleSelection: true,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 1,
+      allowSearchFilter: true,
+      searchPlaceholderText: 'Filter...',
+      closeDropDownOnSelection: true,
+      clearSearchFilter: true,
+      placeholder: 'Select'
+    };
+  }
+
+//#endregion ngOnInit
+
+  //#region getUsers 
+
+getUsers() {
+    this.dataService.getUsers().subscribe(
+      (data: any []) => {
+        data.forEach(user => {
+          if (user.id <= 50) {
+            this.restangularUsers.push(user);
+          }
+        });
+      },
+      err => console.error(err),
+      () => console.log(this.restangularUsers)
+    );
+  }
+
+//#endregion getUsers
+
+  //#region getHttpUsers 
+
+getHttpUsers() {
+    this.dataService.getHttpUsers().subscribe(
+      (data:any []) => {
+        data.forEach(user => {
+          if (user.id > 25 && user.id <= 50) {
+          this.httpUsers.push(user);
+          }
+        });
+      },
+      err => console.error(err),
+      () => console.log(this.httpUsers)
+    );
+  }
+
+//#endregion getHttpUsers
+
+//#region onPageChange 
+
+public onPageChange(pageNum: number): void {
+  this.pageSize = this.itemsPerPage*(pageNum - 1);
+  this.currentPage = pageNum;
+}
+
+/**
+ * changePagesize
+pageSize: number */
+public changePagesize(num: number): void {
+  this.itemsPerPage = this.pageSize + num;
+  this.ChangePageSize = true;
+}
+
+
+onItemSelect(item: any) {
+  console.log(item);
+}
+onSelectAll(items: any) {
+  console.log(items);
+}
+
+//#endregion onPageChange
+}
